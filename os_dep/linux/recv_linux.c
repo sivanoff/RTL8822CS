6fa96790529128cbc7b02e253993be56fb91cff3
sivanoff <info@stephan.i9x.ru>
2022-04-15 19:44:47 +0300

    Fix GRO_DROP

--- os_dep/linux/recv_linux.c
+++ os_dep/linux/recv_linux.c
@@ -355,8 +355,8 @@ static int napi_recv(_adapter *padapter, int budget)
 
 #ifdef CONFIG_RTW_GRO
 		if (pregistrypriv->en_gro) {
-			if (rtw_napi_gro_receive(&padapter->napi, pskb) != GRO_DROP)
-				rx_ok = _TRUE;
+			rtw_napi_gro_receive(&padapter->napi, pskb);
+			rx_ok = _TRUE;
 			goto next;
 		}
 #endif /* CONFIG_RTW_GRO */
